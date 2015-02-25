0aa5f537d51d731a73c3a6ae24bd8ab5e63fa96b
Jules Lui <juleslui@yahoo.com>
2015-02-25 15:20:15 +0800

    Added activesupport

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  attr_accessible :username, :last_name, :first_name, :middle_initial, :admin_check, :office_name, :email, :encrypted_password
 end
