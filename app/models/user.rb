96e7f35efcc2ab433d08b3ec4250c4d2befcc20a
Jules Lui <juleslui@yahoo.com>
2015-02-25 14:54:32 +0800

    Added more devise files

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessible :username, :last_name, :first_name, :middle_initial, :admin_check, :office_name, :email, :encrypted_password
 end
